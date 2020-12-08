a04016a5050245a19b386ea2bdbda0f85d257616
DeepakNithin <9918004210@klu.ac.in>
2020-12-08 21:36:29 +0530

    Coffee Machine program

---
+++ Day13/Day13 Challenge.py
@@ -0,0 +1,10 @@
+class CoffeeMachine:
+    def __init__(self,Water,Milk,Coffee,Money):
+        self.Water = Water
+        self.Milk = Milk
+        self.Coffee = Coffee
+        self.Money = Money
+    def resources(self,choice,beverage):
+        self.beverage = beverage
+        self.choice = choice
+        
